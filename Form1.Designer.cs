ea86696335d42c23ca9b8e1c5e5b32ba330422c8
hazim017 <671661@student.inholland.nl>
2025-01-12 17:08:23 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace rekenmachiene
+{
+    partial class Telraam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNR1 = new System.Windows.Forms.Button();
+            this.btnNR2 = new System.Windows.Forms.Button();
+            this.btnNR3 = new System.Windows.Forms.Button();
+            this.btnNR4 = new System.Windows.Forms.Button();
+            this.btnNR5 = new System.Windows.Forms.Button();
+            this.btnNR6 = new System.Windows.Forms.Button();
+            this.btnNr7 = new System.Windows.Forms.Button();
+            this.btnNR8 = new System.Windows.Forms.Button();
+            this.btnNR9 = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnKeer = new System.Windows.Forms.Button();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.btnNR0 = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnis = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnPunt = new System.Windows.Forms.Button();
+            this.txtrekenen = new System.Windows.Forms.TextBox();
+            this.btnTotDeMacht = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNR1
+            // 
+            this.btnNR1.BackColor = System.Drawing.SystemColors.Control;
+            this.btnNR1.Location = new System.Drawing.Point(59, 432);
+            this.btnNR1.Name = "btnNR1";
+            this.btnNR1.Size = new System.Drawing.Size(86, 35);
+            this.btnNR1.TabIndex = 0;
+            this.btnNR1.Text = "1";
+            this.btnNR1.UseVisualStyleBackColor = false;
+            this.btnNR1.Click += new System.EventHandler(this.btnNR1_Click);
+            // 
+            // btnNR2
+            // 
+            this.btnNR2.Location = new System.Drawing.Point(161, 432);
+            this.btnNR2.Name = "btnNR2";
+            this.btnNR2.Size = new System.Drawing.Size(88, 35);
+            this.btnNR2.TabIndex = 1;
+            this.btnNR2.Text = "2";
+            this.btnNR2.UseVisualStyleBackColor = true;
+            this.btnNR2.Click += new System.EventHandler(this.btnNR2_Click);
+            // 
+            // btnNR3
+            // 
+            this.btnNR3.Location = new System.Drawing.Point(271, 432);
+            this.btnNR3.Name = "btnNR3";
+            this.btnNR3.Size = new System.Drawing.Size(92, 35);
+            this.btnNR3.TabIndex = 2;
+            this.btnNR3.Text = "3";
+            this.btnNR3.UseVisualStyleBackColor = true;
+            this.btnNR3.Click += new System.EventHandler(this.btnNR3_Click);
+            // 
+            // btnNR4
+            // 
+            this.btnNR4.Location = new System.Drawing.Point(59, 376);
+            this.btnNR4.Name = "btnNR4";
+            this.btnNR4.Size = new System.Drawing.Size(86, 39);
+            this.btnNR4.TabIndex = 3;
+            this.btnNR4.Text = "4";
+            this.btnNR4.UseVisualStyleBackColor = true;
+            this.btnNR4.Click += new System.EventHandler(this.btnNR4_Click);
+            // 
+            // btnNR5
+            // 
+            this.btnNR5.Location = new System.Drawing.Point(161, 376);
+            this.btnNR5.Name = "btnNR5";
+            this.btnNR5.Size = new System.Drawing.Size(88, 39);
+            this.btnNR5.TabIndex = 4;
+            this.btnNR5.Text = "5";
+            this.btnNR5.UseVisualStyleBackColor = true;
+            this.btnNR5.Click += new System.EventHandler(this.btnNR5_Click);
+            // 
+            // btnNR6
+            // 
+            this.btnNR6.Location = new System.Drawing.Point(271, 376);
+            this.btnNR6.Name = "btnNR6";
+            this.btnNR6.Size = new System.Drawing.Size(92, 39);
+            this.btnNR6.TabIndex = 5;
+            this.btnNR6.Text = "6";
+            this.btnNR6.UseVisualStyleBackColor = true;
+            this.btnNR6.Click += new System.EventHandler(this.btnNR6_Click);
+            // 
+            // btnNr7
+            // 
+            this.btnNr7.Location = new System.Drawing.Point(59, 323);
+            this.btnNr7.Name = "btnNr7";
+            this.btnNr7.Size = new System.Drawing.Size(86, 34);
+            this.btnNr7.TabIndex = 6;
+            this.btnNr7.Text = "7";
+            this.btnNr7.UseVisualStyleBackColor = true;
+            this.btnNr7.Click += new System.EventHandler(this.btnNr7_Click);
+            // 
+            // btnNR8
+            // 
+            this.btnNR8.Location = new System.Drawing.Point(161, 323);
+            this.btnNR8.Name = "btnNR8";
+            this.btnNR8.Size = new System.Drawing.Size(88, 34);
+            this.btnNR8.TabIndex = 7;
+            this.btnNR8.Text = "8";
+            this.btnNR8.UseVisualStyleBackColor = true;
+            this.btnNR8.Click += new System.EventHandler(this.btnNR8_Click);
+            // 
+            // btnNR9
+            // 
+            this.btnNR9.Location = new System.Drawing.Point(271, 323);
+            this.btnNR9.Name = "btnNR9";
+            this.btnNR9.Size = new System.Drawing.Size(92, 34);
+            this.btnNR9.TabIndex = 8;
+            this.btnNR9.Text = "9";
+            this.btnNR9.UseVisualStyleBackColor = true;
+            this.btnNR9.Click += new System.EventHandler(this.btnNR9_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(395, 323);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(92, 34);
+            this.btnDivide.TabIndex = 9;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnKeer
+            // 
+            this.btnKeer.Location = new System.Drawing.Point(395, 376);
+            this.btnKeer.Name = "btnKeer";
+            this.btnKeer.Size = new System.Drawing.Size(92, 31);
+            this.btnKeer.TabIndex = 10;
+            this.btnKeer.Text = "x";
+            this.btnKeer.UseVisualStyleBackColor = true;
+            this.btnKeer.Click += new System.EventHandler(this.btnKeer_Click);
+            // 
+            // btnMin
+            // 
+            this.btnMin.Location = new System.Drawing.Point(395, 432);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(92, 35);
+            this.btnMin.TabIndex = 11;
+            this.btnMin.Text = "-";
+            this.btnMin.UseVisualStyleBackColor = true;
+            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            // 
+            // btnNR0
+            // 
+            this.btnNR0.Location = new System.Drawing.Point(59, 496);
+            this.btnNR0.Name = "btnNR0";
+            this.btnNR0.Size = new System.Drawing.Size(190, 39);
+            this.btnNR0.TabIndex = 12;
+            this.btnNR0.Text = "0";
+            this.btnNR0.UseVisualStyleBackColor = true;
+            this.btnNR0.Click += new System.EventHandler(this.btnNR0_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(124, 554);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(92, 39);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "c";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnis
+            // 
+            this.btnis.Location = new System.Drawing.Point(271, 554);
+            this.btnis.Name = "btnis";
+            this.btnis.Size = new System.Drawing.Size(92, 39);
+            this.btnis.TabIndex = 14;
+            this.btnis.Text = "=";
+            this.btnis.UseVisualStyleBackColor = true;
+            this.btnis.Click += new System.EventHandler(this.btnis_Click);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(395, 496);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(92, 39);
+            this.btnPlus.TabIndex = 15;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnPunt
+            // 
+            this.btnPunt.Location = new System.Drawing.Point(271, 496);
+            this.btnPunt.Name = "btnPunt";
+            this.btnPunt.Size = new System.Drawing.Size(92, 39);
+            this.btnPunt.TabIndex = 16;
+            this.btnPunt.Text = ".";
+            this.btnPunt.UseVisualStyleBackColor = true;
+            this.btnPunt.Click += new System.EventHandler(this.btnPunt_Click);
+            // 
+            // txtrekenen
+            // 
+            this.txtrekenen.Location = new System.Drawing.Point(59, 70);
+            this.txtrekenen.Name = "txtrekenen";
+            this.txtrekenen.Size = new System.Drawing.Size(399, 26);
+            this.txtrekenen.TabIndex = 19;
+            this.txtrekenen.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnTotDeMacht
+            // 
+            this.btnTotDeMacht.Location = new System.Drawing.Point(395, 263);
+            this.btnTotDeMacht.Name = "btnTotDeMacht";
+            this.btnTotDeMacht.Size = new System.Drawing.Size(92, 33);
+            this.btnTotDeMacht.TabIndex = 20;
+            this.btnTotDeMacht.Text = "^";
+            this.btnTotDeMacht.UseVisualStyleBackColor = true;
+            this.btnTotDeMacht.Click += new System.EventHandler(this.btnTotDeMacht_Click);
+            // 
+            // Telraam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(499, 617);
+            this.Controls.Add(this.btnTotDeMacht);
+            this.Controls.Add(this.txtrekenen);
+            this.Controls.Add(this.btnPunt);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.btnis);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnNR0);
+            this.Controls.Add(this.btnMin);
+            this.Controls.Add(this.btnKeer);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnNR9);
+            this.Controls.Add(this.btnNR8);
+            this.Controls.Add(this.btnNr7);
+            this.Controls.Add(this.btnNR6);
+            this.Controls.Add(this.btnNR5);
+            this.Controls.Add(this.btnNR4);
+            this.Controls.Add(this.btnNR3);
+            this.Controls.Add(this.btnNR2);
+            this.Controls.Add(this.btnNR1);
+            this.Name = "Telraam";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNR1;
+        private System.Windows.Forms.Button btnNR2;
+        private System.Windows.Forms.Button btnNR3;
+        private System.Windows.Forms.Button btnNR4;
+        private System.Windows.Forms.Button btnNR5;
+        private System.Windows.Forms.Button btnNR6;
+        private System.Windows.Forms.Button btnNr7;
+        private System.Windows.Forms.Button btnNR8;
+        private System.Windows.Forms.Button btnNR9;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnKeer;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.Button btnNR0;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnis;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnPunt;
+        private System.Windows.Forms.TextBox txtrekenen;
+        private System.Windows.Forms.Button btnTotDeMacht;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rekenmachiene
+{
+    public partial class Telraam : Form
+    {
+        public Telraam()
+        {
+            InitializeComponent(); // Initialiseer de componenten van de form
+        }
+        string operatie; // Variabele om de geselecteerde operatie op te slaan
+        int Eerstenummer; // Variabele om het eerste nummer op te slaan
+        private void btnPunt_Click(object sender, EventArgs e)
+        {
+            // Controleer of er al een komma aanwezig is in het tekstveld
+            char[] letters = { ',' };
+
+            bool containsAnyLetter = txtrekenen.Text.IndexOfAny(letters) >=0;
+            if (containsAnyLetter == true)
+            {
+                // Toon een foutmelding als er al een komma is
+                MessageBox.Show("your word contains comma");
+            }
+            else
+            {
+                // Voeg een komma toe aan het tekstveld
+                txtrekenen.Text += ",";
+            }
+
+        }
+        // Methodes voor elk cijferknopje om het bijbehorende nummer aan het tekstveld toe te voegen
+        private void btnNr7_Click(object sender, EventArgs e)
+        {
+            txtrekenen.Text += "7";
+        }
+
+        private void btnNR8_Click(object sender, EventArgs e)
+        {
+            txtrekenen.Text += "8";
+        }
+
+        private void btnNR9_Click(object sender, EventArgs e)
+        {
+            txtrekenen.Text += "9";
+        }
+
+        private void btnNR4_Click(object sender, EventArgs e)
+        {
+            txtrekenen.Text += "4";
+        }
+
+        private void btnNR5_Click(object sender, EventArgs e)
+        {
+            txtrekenen.Text += "5";
+        }
+
+        private void btnNR6_Click(object sender, EventArgs e)
+        {
+            txtrekenen.Text += "6";
+        }
+
+        private void btnNR1_Click(object sender, EventArgs e)
+        {
+            txtrekenen.Text += "1";
+        }
+
+        private void btnNR2_Click(object sender, EventArgs e)
+        {
+            txtrekenen.Text += "2";
+        }
+
+        private void btnNR3_Click(object sender, EventArgs e)
+        {
+            txtrekenen.Text += "3";
+        }
+
+        private void btnNR0_Click(object sender, EventArgs e)
+        {
+            txtrekenen.Text += "0";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Maak het tekstveld leeg
+            txtrekenen.Text = " ";
+        }
+        // Methode om een operatie (zoals optellen of aftrekken) te selecteren
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            Eerstenummer = Convert.ToInt32(txtrekenen.Text); // Sla het eerste nummer op
+            txtrekenen.ResetText(); // Maak het tekstveld leeg
+            operatie = "+";  // Stel de operatie in
+
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            Eerstenummer = Convert.ToInt32(txtrekenen.Text);
+            txtrekenen.ResetText();
+            operatie = "-";
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            Eerstenummer = Convert.ToInt32(txtrekenen.Text);
+            txtrekenen.ResetText();
+            operatie = "/";
+        }
+        private void btnKeer_Click(object sender, EventArgs e)
+        {
+            Eerstenummer = Convert.ToInt32(txtrekenen.Text);
+            txtrekenen.ResetText();
+            operatie = "x";
+        }
+        private void btnTotDeMacht_Click(object sender, EventArgs e)
+        {
+            Eerstenummer = Convert.ToInt32(txtrekenen.Text);
+            txtrekenen.ResetText();
+            operatie = "^"; // Operatie voor machtsverheffing
+        }
+        private void btnis_Click(object sender, EventArgs e)
+        {
+            int Tweedenummer = Convert.ToInt32(txtrekenen.Text);  // Sla het tweede nummer op
+            int Resultaat; // Variabele voor het resultaat
+
+            if (operatie == "+")
+            {
+                // Voer optelling uit
+                Resultaat = Eerstenummer + Tweedenummer;
+                txtrekenen.Text = Resultaat.ToString();
+                Eerstenummer = Resultaat;  // Update het eerste nummer
+
+            }
+            if (operatie == "-")
+            {
+                // Voer aftrekking uit
+                Resultaat = Eerstenummer - Tweedenummer;
+                txtrekenen.Text = Resultaat.ToString();
+                Eerstenummer = Resultaat;
+            }
+            if (operatie == "x")
+            {
+                // Voer vermenigvuldiging uit
+                Resultaat = Eerstenummer * Tweedenummer;
+                txtrekenen.Text = Resultaat.ToString();
+                Eerstenummer = Resultaat;
+            }
+            if (operatie == "/")
+            {
+                if (Tweedenummer != 0)
+                {
+                    // Voer deling uit als de deler niet nul is
+                    Resultaat = Eerstenummer / Tweedenummer;
+                    txtrekenen.Text = Resultaat.ToString();
+                    Eerstenummer = Resultaat;
+                }
+                else
+                {
+                    // Toon foutmelding als er gedeeld wordt door nul
+                    MessageBox.Show("error");
+                }
+             if (operatie == "^")
+                {
+                    // Voer machtsverheffing uit (vierkantswortel)
+                    Resultaat = Eerstenummer * Eerstenummer;
+                    txtrekenen.Text = Resultaat.ToString();
+                    Eerstenummer = Resultaat;
+                }
+
+            }
+        }
+    }
+}
